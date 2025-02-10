d016a9b8284b308d91fa6ff3151bd2408503467b
Adam Casto <adam@iptic.com>
2025-02-10 17:50:02 -0500

    Added stubs of RegisterUserCommandsAction and RegisterAssistantCommandsAction to the project actions directory so that an error isn't given when they don't exist and the user can copy them for their own use.

---
+++ actions/register_assistant_commands_action.py
@@ -0,0 +1,31 @@
+from session_handler import InteractionAction
+
+
+class RegisterAssistantCommandsAction(InteractionAction):
+    """
+    Default implementation for registering custom assistant commands.
+    Users can copy this to their user actions directory and modify it to add custom commands.
+    """
+
+    def __init__(self, session):
+        self.session = session
+
+    def run(self, args=None):
+        """
+        Return a dictionary of custom commands to register.
+
+        Returns:
+            dict: Mapping of command names to command configurations.
+                 Example format:
+                 {
+                     "COMMAND": {
+                         "args": ["arg1", "arg2"],
+                         "auto_submit": True,
+                         "function": {
+                             "type": "action",
+                             "name": "action_name"
+                         }
+                     }
+                 }
+        """
+        return {}
---
+++ actions/register_user_commands_action.py
@@ -0,0 +1,31 @@
+from session_handler import InteractionAction
+
+
+class RegisterUserCommandsAction(InteractionAction):
+    """
+    Default implementation for registering custom user commands.
+    Users can copy this to their user actions directory and modify it to add custom commands.
+    """
+
+    def __init__(self, session):
+        self.session = session
+
+    def run(self, args=None):
+        """
+        Return a dictionary of custom commands to register.
+
+        Returns:
+            dict: Mapping of command names to command configurations.
+                 Example format:
+                 {
+                     "command_name": {
+                         "description": "Command description",
+                         "function": {
+                             "type": "action",
+                             "name": "action_name",
+                             "args": ["optional", "args"]
+                         }
+                     }
+                 }
+        """
+        return {}
